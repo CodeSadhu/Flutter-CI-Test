aeadceeb28201150ddb04bd2f58f1fae29758760
Atharva Patwardhan <patwardhanatharva6@gmail.com>
2020-08-29 20:53:05 +0530

    Add travis.sh

---
+++ travis.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+if  [[ -n "$(type -t flutter)" ]]; then
+  : ${FLUTTER:=flutter}
+fi
+echo "== FLUTTER: $FLUTTER"
+
+FLUTTER_VERS=`$FLUTTER --version | head -1`
+echo "== FLUTTER_VERS: $FLUTTER_VERS"
+
+set -x;
+$FLUTTER analyze;
+$FLUTTER format --dry-run --set-exit-if-changed .;
+$FLUTTER test
+
+echo "== END OF TESTS"
